ac35315fe3317c542c6712d9b7f7002adc7d3d5c
bao-portabl <bao@getportabl.com>
2023-04-05 12:15:49 -0700

    init js-sync-with-portabl

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ['custom'],
+  rules: {
+    'no-console': 'warn',
+  },
   settings: {
     next: {
       rootDir: ['apps/*/'],
