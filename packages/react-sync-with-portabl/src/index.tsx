05162a2af2609963830296b121b0672047aea254
bao-portabl <bao@getportabl.com>
2023-04-06 19:36:13 -0700

    create wrapper around react for vanilla-js sdk

--- packages/react-sync-with-portabl/src/index.tsx
+++ packages/react-sync-with-portabl/src/index.tsx
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { createSyncWithPortabl } from '@portabl/js-sync-with-portabl';
 
-const SyncWithPortabl = () => <div>todo</div>;
+interface SyncWithPortablProps {
+  domain: string;
+  audience: string;
+  clientId: string;
+  passportUrl: string;
+  getPrereqs: () => Promise<{
+    isSynced: boolean;
+    datapoints: Array<Record<string, any>>;
+  }>;
+  onUserConsent: () => Promise<void>;
+}
+
+const SyncWithPortabl = ({
+  domain,
+  audience,
+  clientId,
+  passportUrl,
+  getPrereqs,
+  onUserConsent,
+}: SyncWithPortablProps) => {
+  const syncWrapperRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      if (syncWrapperRef.current) {
+        const syncWithPortablElement = await createSyncWithPortabl({
+          domain,
+          audience,
+          clientId,
+          passportUrl,
+          getPrereqs,
+          onUserConsent,
+        });
+
+        syncWrapperRef.current.appendChild(syncWithPortablElement);
+      }
+    })();
+
+    return () => {
+      if (syncWrapperRef.current) {
+        syncWrapperRef.current.innerHTML = '';
+      }
+    };
+  }, [domain, audience, clientId, passportUrl]);
+
+  return <div ref={syncWrapperRef} />;
+};
 
 export default SyncWithPortabl;
