8836fc86f4fb16bff4e564b35056e0663e2fa04c
bao-portabl <bao@getportabl.com>
2023-04-19 15:31:01 -0700

    add util function for handling retries

---
+++ packages/js-sync-with-portabl/src/lib/fetchRetries.ts
@@ -0,0 +1,25 @@
+export async function fetchRetries<T>(
+  fn: () => Promise<T>,
+  maxRetry: number,
+): Promise<T> {
+  let retryCount = 0;
+  let delay = 1000;
+
+  const retry = async (): Promise<T> => {
+    try {
+      return await fn();
+    } catch (error) {
+      if (retryCount < maxRetry) {
+        retryCount += 1;
+        delay *= 2;
+        await new Promise(resolve => {
+          setTimeout(resolve, delay);
+        });
+        return retry();
+      }
+      throw new Error('Max retry reached');
+    }
+  };
+
+  return retry();
+}
