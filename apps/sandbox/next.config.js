8ba6eee63ae6b8812317c3fb4533fbe731326933
bao-portabl <bao@getportabl.com>
2023-04-24 12:43:14 -0700

    kill apps folder

--- apps/sandbox/next.config.js
+++
@@ -1,8 +0,0 @@
-const withTM = require('next-transpile-modules')([
-  '@portabl/js-connect-with-portabl',
-  '@portabl/react-connect-with-portabl',
-]);
-
-module.exports = withTM({
-  reactStrictMode: true,
-});
